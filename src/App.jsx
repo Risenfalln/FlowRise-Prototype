bee7b8c3ae8d46eb9e56732f65e69748034f867e
Risenfalln <randgamesetc@hotmail.com>
2025-09-02 12:43:01 +0000

    f

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import BottomNav from "./components/ui/BottomNav";
 import Today from "./features/jobs/Today";
 import JobsList from "./features/jobs/JobsList";
-//import JobDetail from "./features/jobs/JobDetail";
+import JobDetail from "./features/jobs/JobDetail";
 import AssistantHome from "./features/assistant/AssistantHome";
 import AssistantPanel from "./features/assistant/AssistantPanel";
 import Library from "./features/library/Library";
